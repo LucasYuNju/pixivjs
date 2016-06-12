1a6aa379bb34e0855a420643e57fa7faf2d6c4b3
LucasYuNju <LucasYuNju@gmail.com>
2016-06-12 22:13:42 +0800

    use fetch instead of request.

--- api.js
+++ api.js
@@ -1,41 +1,77 @@
-const request = require("request");
+const fetch = require("isomorphic-fetch");
+const querystr = require("querystring");
 
 class API {
     constructor (props) {
-        this.cookieJar = request.jar();
         this.debug = props.debug;
     }
 
+    _toQueryString(obj) {
+        return obj ? Object.keys(obj).sort().map(function (key) {
+            var val = obj[key];
+            if (Array.isArray(val)) {
+                return val.sort().map(function (val2) {
+                    return encodeURIComponent(key) + '=' + encodeURIComponent(val2);
+                }).join('&');
+            }
+            return encodeURIComponent(key) + '=' + encodeURIComponent(val);
+        }).join('&') : '';
+    }
+
     _request (method = "GET", url, { params = {}, headers = {}, body = {} }) {
+        if(Object.keys(params).length !== 0) {
+            url += "?" + this._toQueryString(params);
+        }
         headers["Refer"] = this.URLS.REFER;
         headers["User-Agent"] = "PixivIOSApp/5.8.3";
+        headers["Content-Type"] = "application/x-www-form-urlencoded";
         const options = {
-            url,
             method,
             headers,
-            form: body,
-            jar: this.cookieJar,
-            qs: params,
+            credentials: 'same-origin',
+            body: querystr.stringify(body),
         };
 
         return new Promise((resolve, reject) => {
-            request(options, (error, response, body) => {
-                if(error) {
-                    reject(error);
-                }
-                if(this.debug) {
-                    console.log(method, " ", url);
-                    console.log("==>", body.substr(0, 400), "\n");
-                }
-                if(response.headers["content-type"] !== "application/json") {
-                    reject(`Error, response is not applicaiton/json, status: ${response.status}`);
-                }
-                else {
-                    const json = JSON.parse(body);
-                    resolve(json);
-                }
-            });
+            fetch(url, options)
+                .then(response => {
+                    if(response.headers.get("set-cookie")) {
+                        // console.log(response.headers.get("set-cookie"));
+                        // this.cookieJar = cookie.parse(response.headers.get("set-cookie"));
+                        // console.log(this.cookieJar);
+                    }
+                    return response;
+                })
+                .then(response => response.text())
+                .then(text => {
+                    if(this.debug) {
+                        console.log(method, url);
+                        console.log(text.substr(0, 200) + "\n");
+                    }
+                    const body = JSON.parse(text);
+                    resolve(body);
+                })
+                .catch(reject);
         });
+
+        // return new Promise((resolve, reject) => {
+        //     request(options, (error, response, body) => {
+        //         if(error) {
+        //             reject(error);
+        //         }
+        //         if(this.debug) {
+        //             console.log(method, " ", url);
+        //             console.log("==>", body.substr(0, 400), "\n");
+        //         }
+        //         if(response.headers["content-type"] !== "application/json") {
+        //             reject(`Error, response is not applicaiton/json, status: ${response.status}`);
+        //         }
+        //         else {
+        //             const json = JSON.parse(body);
+        //             resolve(json);
+        //         }
+        //     });
+        // });
     }
 
     /**
--- test/test.js
+++ test/test.js
@@ -1,52 +1,52 @@
 const config = require("../config/account");
 const API = require("../api");
 
-config.verbose = false;
+config.debug = true;
 const api = new API(config);
 
 api
     .login(config.username, config.password)
     .then(result => {
-
+        // console.log(result);
         api
             .ranking()
             .then(response => {
-              console.log(response);
+            //   console.log(JSON.stringify(response).substr(0, 100));
             })
             .catch(console.error);
 
         api
             .meFeeds()
             .then(response => {
-              console.log(response);
+            //   console.log(response);
             })
             .catch(console.error);
 
         api
             .userWorks(1184799)
             .then(response => {
-              console.log(response);
+            //   console.log(response);
             })
             .catch(console.error);
 
         api
             .users(1184799)
             .then(response => {
-              console.log(response);
+            //   console.log(response);
             })
             .catch(console.error);
 
         api
             .works(46363414)
             .then(response => {
-              console.log(reponse);
+            //   console.log(reponse);
             })
             .catch(console.error);
 
         api
             .meFavoriteWorks()
             .then(response => {
-              console.log(response);
+            //   console.log(response);
             })
             .catch(console.error);
 
